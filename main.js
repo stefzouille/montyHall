014cfa60971d5c9e43899134771d5454c38bafde
stefzouille <contact.lokcy@gmail.com>
2021-04-08 17:22:03 +0200

    add

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ while (tab1 !== -1) {
     const n = randomInt(1, 4) 
 
     //choisi une porte chevre au hasard assigné a variable car
-    let car = gates[n].split('')
+    let car = gates[n]
 
     //remplace le mot goat du random par le mot car au hasard
     gates[randomInt] = "car"
@@ -23,16 +23,22 @@ while (tab1 !== -1) {
     switch (input) {
       
       case 0:
-        const porte1 = readlineSync.question(`tu as choisi la porte 1 \n donc la porte ${gates[n]} il y a une chèvre\n changer ou garder? ` )
-        let choix = ['garder', 'changer']
-        const input = readlineSync.keyInSelect(choix, `Que veut tu faire ?  \n ` )
-        
+        const porte1 = readlineSync.question(`tu as choisi la porte 1 \n donc la porte ${randomInt(1, 4) } il y a une chèvre\n changer ou garder? ` )
+        let choix1 = ['garder', 'changer']
+        const input1 = readlineSync.keyInSelect(choix1, `Que veut tu faire ?  \n ` )
+        console.log(`la porte ${input1} il y a la ${n}`)
       break
       case 1:
-        const porte2= readlineSync.question(`tu as choisi la porte 2 \n donc la porte ${gates[n]} il y a une chèvre\n ` )
+        const porte2= readlineSync.question(`tu as choisi la porte 2 \n donc la porte ${randomInt(1, 4)} il y a une chèvre\n ` )
+         let choix2 = ['garder', 'changer']
+        const input2 = readlineSync.keyInSelect(choix2, `Que veut tu faire ?  \n ` )
+        console.log(`la porte ${input2} il y a la ${n}`)
       break
       case 2:
-        const poret3 = readlineSync.question(`tu as choisi la porte 3 \n donc la porte ${gates[n]} il y a une chèvre\n ` )
+        const poret3 = readlineSync.question(`tu as choisi la porte 3 \n donc la porte ${randomInt(1, 4)} il y a une chèvre\n ` )
+         let choix3 = ['garder', 'changer']
+        const input3 = readlineSync.keyInSelect(choix3, `Que veut tu faire ?  \n ` )
+        console.log(`la ${input3} il y a la ${n}`)
       break
     }
 
