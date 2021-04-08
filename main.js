8d33482ed761f197223cd73a330c3090dcc227ca
stefzouille <contact.lokcy@gmail.com>
2021-04-08 17:35:49 +0200

    add

--- main.js
+++ main.js
@@ -23,62 +23,25 @@ while (tab1 !== -1) {
     switch (input) {
       
       case 0:
-        const porte1 = readlineSync.question(`tu as choisi la porte 1 \n donc la porte ${randomInt(1, 4) } il y a une chèvre\n changer ou garder? ` )
+        const porte1 = readlineSync.question(`tu as choisi la porte 1 \n donc la porte ${randomInt(1, 4)} il n y a pas une chèvre\n ` )
         let choix1 = ['garder', 'changer']
         const input1 = readlineSync.keyInSelect(choix1, `Que veut tu faire ?  \n ` )
-        console.log(`la porte ${input1} il y a la ${n}`)
+        console.log(`la${car} il y a la `)
       break
       case 1:
-        const porte2= readlineSync.question(`tu as choisi la porte 2 \n donc la porte ${randomInt(1, 4)} il y a une chèvre\n ` )
+        const porte2= readlineSync.question(`tu as choisi la porte 2 \n donc la porte ${randomInt(1, 4)} il n y a pas une chèvre\n ` )
          let choix2 = ['garder', 'changer']
         const input2 = readlineSync.keyInSelect(choix2, `Que veut tu faire ?  \n ` )
-        console.log(`la porte ${input2} il y a la ${n}`)
+        console.log(`la${car} il y a la `)
       break
       case 2:
-        const poret3 = readlineSync.question(`tu as choisi la porte 3 \n donc la porte ${randomInt(1, 4)} il y a une chèvre\n ` )
+        const poret3 = readlineSync.question(`tu as choisi la porte 3 \n donc la porte ${randomInt(1, 4)} il n y a pas une chèvre\n ` )
          let choix3 = ['garder', 'changer']
         const input3 = readlineSync.keyInSelect(choix3, `Que veut tu faire ?  \n ` )
-        console.log(`la ${input3} il y a la ${n}`)
+        console.log(`la${car} il y a la `)
       break
     }
-
-    //console.log(`Tu as choisis une porte${input} `)
-
-
-    
-    
-    //let index = word.indexOf(gates)
-    
-  // let choice = ["gates1", "gates2", "gates3"]
-  
-  //   const input = readlineSync.keyInSelect(choice, `Quel portes choisies tu ? \n ` )
-
-  //   const choix1 = readlineSync.question(`tu as choisis la porte ${choix1} il y as une chévre derriere la porte ${choice[n]} ` )
-  //   if (input !== choix1) {
-
-  //     const choix3 = readlineSync.question(`tu garde ta porte ${input} ou tu choisis l'autre porte cachée ? ` )
-  //   }
-    
-
-  //   console.log(input)
-
     process.exit(1)
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   }           
 }
  startGame()
